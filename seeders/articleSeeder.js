e897e63b06b5e9931d46b570591b5ac823470761
Triana Misa <trianamisa@gmail.com>
2022-05-15 19:09:30 -0300

    agrego img y userId al seeder de articulos

--- seeders/articleSeeder.js
+++ seeders/articleSeeder.js
@@ -1,6 +1,14 @@
 const { faker } = require("@faker-js/faker");
 const { Article } = require("../models");
 
+const rn = require("random-number");
+const options = {
+  min: 0,
+  max: 100,
+  integer: true,
+};
+rn(options);
+
 faker.locale = "es";
 // capaz podemos require la table de users y agregarlo random
 module.exports = async () => {
@@ -9,7 +17,9 @@ module.exports = async () => {
   for (let i = 0; i < 100; i++) {
     articles.push({
       title: faker.lorem.sentence(5),
+      img: "https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png",
       content: faker.lorem.paragraphs(),
+      userId: rn(options),
     });
   }
   await Article.bulkCreate(articles);
