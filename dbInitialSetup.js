af832bfc9871a8c768e3d70ee5b9d11748961bd4
Zcehtro <stefano.n.giaimo@gmail.com>
2022-05-17 12:03:28 -0300

    Agregar mensajes del servidor en la consola

--- dbInitialSetup.js
+++ dbInitialSetup.js
@@ -3,6 +3,7 @@ const db = require("./models");
 module.exports = async () => {
   // Crear tablas:
   await db.sequelize.sync({ force: true });
+  console.log("[Server] Inicializando archivos! NO NAVEGAR!!!");
   console.log("[Database] ¡Las tablas fueron creadas!");
 
   // Ejecutar seeders (datos de prueba):
@@ -10,4 +11,5 @@ module.exports = async () => {
   await require("./seeders/articleSeeder")();
   await require("./seeders/commentSeeder")();
   console.log("[Database] ¡Los datos de prueba fueron insertados!");
+  console.log("[Server] Carga completada! Se puede navegar!");
 };
